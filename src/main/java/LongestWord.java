dfbc9f7b4c9cd91a67082bf1da3733a576b00576
Dmitriy_Suslov <dm_itry@tut.by>
2018-01-12 15:43:57 +0300

    11

--- src/main/java/LongestWord.java
+++ src/main/java/LongestWord.java
@@ -14,6 +14,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class LongestWord {
 
+
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable> {
 
